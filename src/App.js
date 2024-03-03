7b049a219205a8771b5a28a2e2a97c57ceca453b
Chandan <Chandan17ee@gmail.com>
2024-03-03 22:10:59 +0530

    Chore: integrated nav button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import Loader from "./Loader/Loader";
-const NavLinks = lazy(()=>import('./NavLinks/NavLink'))
+const NavLinks = lazy(() => import("./NavLinks/NavLink"));
 const AccountLogin = lazy(() => import("./Login/Login"));
 const AccountRegister = lazy(() => import("./Register/Register"));
 const Home = lazy(() => import("./HomePage/HomePage"));
--- src/NavLinks/NavLink.js
+++ src/NavLinks/NavLink.js
@@ -6,19 +6,34 @@ const NavLinks = () => {
   return (
     <div>
       <div>
-        <NavLink to="/" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
-          Home
-        </NavLink>
+        <div>
+          <NavLink to="/" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+            Home
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/service" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+            Services
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/contact" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+            contact
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/application" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+            Application
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/todo" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+            ToDo-App
+          </NavLink>
+        </div>
       </div>
       <div>
-        <NavLink to="/application" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
-          Application
-        </NavLink>
-      </div>
-      <div>
-        <NavLink to="/contact" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
-          contact
-        </NavLink>
+        <button>Login</button>
       </div>
     </div>
   );
