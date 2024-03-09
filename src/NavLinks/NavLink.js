568359faadca6231bd0b2173d591da7e95dfea6f
Chandan <Chandan17ee@gmail.com>
2024-03-09 13:15:08 +0530

    Style: styles navlink

--- src/NavLinks/NavLink.js
+++ src/NavLinks/NavLink.js
@@ -1,38 +1,38 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
+import './Nav.css'
 const NavLinks = () => {
   return (
-    <div>
-      <div>
+    <div className="container-link">
+      <div className="nav-link">
         <div>
-          <NavLink to="/" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+          <NavLink to="/" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "static")}>
             Home
           </NavLink>
         </div>
         <div>
-          <NavLink to="/service" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+          <NavLink to="/service" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "static")}>
             Services
           </NavLink>
         </div>
         <div>
-          <NavLink to="/contact" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+          <NavLink to="/contact" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "static")}>
             contact
           </NavLink>
         </div>
         <div>
-          <NavLink to="/application" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+          <NavLink to="/application" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "static")}>
             Application
           </NavLink>
         </div>
         <div>
-          <NavLink to="/todo" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "")}>
+          <NavLink to="/todo" caseSensitive className={({ isActive }) => (isActive ? "isActive" : "static")}>
             ToDo-App
           </NavLink>
         </div>
       </div>
-      <div>
+      <div className="log-out-button">
         <button>Login</button>
       </div>
     </div>
