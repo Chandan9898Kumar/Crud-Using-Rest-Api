6b0ccf659228038cfc4e4e5ad46bf02713bd0fc6
Chandan <Chandan17ee@gmail.com>
2024-03-09 21:54:32 +0530

    chore: added svg image

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,9 +1,18 @@
 import React from "react";
-import './home.css'
+import "./home.css";
+import { HomeImage } from "../Assets/SvgImage";
 const Home = () => {
   return (
     <div className="container">
-      <div className="sub-container">Home page</div>
+      <div className="image-head">
+        <div>
+          <HomeImage width={40} height={40} />
+        </div>
+        <div>
+          <div className="text">Home Page</div>
+          <p className="sub-text">filtering out the data according to given date range.</p>
+        </div>
+      </div>
     </div>
   );
 };
