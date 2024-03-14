b752cbaa3f54ef75bf6be1b4a2516977c7f9deec
Chandan <Chandan17ee@gmail.com>
2024-03-14 19:02:15 +0530

    Feature: integrated testing

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  collectCoverage: true,
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  coverageReporters: ["json", "lcov", "text", "text-summary", "html", "cobertura"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.js"],
+  testTimeout: 300,
+  globals: {
+    globalVar: "a global variable",
+  },
+  snapshotFormat: {
+    printBasicPrototype: false,
+  },
+  moduleFileExtensions: ["js", "jsx"],
+  moduleDirectories: ["node_modules", "bower_components", "shared"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
+  },
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$", "^.+\\.module\\.(css|sass|scss)$"],
+  testPathIgnorePatterns: ["<rootDir>/config/env/", "<rootDir>/node_modules/"],
+  testMatch: ["<rootDir>/src/**/*.(test).{js,jsx,ts,tsx}", "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}"],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import React from "react" 
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, unmountComponentAtNode } from "@testing-library/react";
+import App from "./App";
+import { act } from "react-dom/test-utils";
+import "@testing-library/jest-dom/extend-expect";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component Testing", () => {
+  let container = null;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    // cleanup on exiting
+    // unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  test("Testing container", () => {
+    act(() => {
+      render(<App />, container);
+    });
+    expect(container).toBeDefined();
+  });
 });
