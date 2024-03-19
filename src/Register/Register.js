e81f9b7e7718e72d862312bdf757d1c07211558e
Chandan <Chandan17ee@gmail.com>
2024-03-19 17:09:19 +0530

    chore: started register page

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import "./register.css";
 const AccountRegister = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register-container">
+      <div className="register-form">
+        <h3>Register</h3>
+        <div className="register-user-name">UserName</div>
+        <div className="register-name-filed">
+          <input type="text" placeholder="Name" aria-label="name" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AccountRegister;
