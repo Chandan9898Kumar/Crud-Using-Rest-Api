011cfb9a0c666a4944e9fd58867d08689ba5df8e
Chandan <Chandan17ee@gmail.com>
2024-03-19 12:20:53 +0530

    chore: started login form

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import "./login.css";
 const AccountLogin = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login-container">
+      <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
+      </div>
+      <div className="form">
+        <h3>Login Here</h3>
+        <div>UserName</div>
+        <div>
+          <input />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AccountLogin;
