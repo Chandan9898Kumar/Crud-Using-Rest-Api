d89e44211a57f0fe6291ce344a2e057371b15581
Chandan <Chandan17ee@gmail.com>
2024-03-24 16:28:16 +0530

    chore: added data in env file

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ErrorBoundary from "./ErrorBoundry";
+import ErrorBoundary from "./ErrorBoundary";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//  To remove console.log in the production build of a React application created. when it is running in production then no console.log will be visible nut in development it will.
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
 root.render(
   <React.StrictMode>
     <ErrorBoundary fallback="Something Went Wrong...">
       <App />
     </ErrorBoundary>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
