c556f7a6ff60eb6fe35f73a7294d3855dd574e3e
Chandan <Chandan17ee@gmail.com>
2024-03-27 16:44:57 +0530

    chore: added login and register api

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,11 +1,48 @@
-import React, { useRef, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from "react";
 import "./login.css";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+axios.interceptors.request.use(function (config) {
+  config.headers["X-Binarybox-Api-Key"] = process.env.REACT_APP_API_KEY;
+  return config;
+});
+
 const AccountLogin = () => {
+  const navigate = useNavigate();
   const emailRef = useRef("");
   const passwordRef = useRef("");
+  const [validationErrors, setValidationErrors] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleChange = () => {};
+  const handleSubmit = () => {
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    setIsSubmitting(true);
+    try {
+      axios
+        .post("/api/login", payload)
+        .then((response) => {
+          localStorage.setItem("token", response.data.token);
+          setValidationErrors("");
+          navigate("/");
+        })
+        .catch((error) => {
+          if (error.response.data.message !== undefined) {
+            setValidationErrors(error.response.data.message);
+          }
+        });
+    } catch {
+      setValidationErrors("Something Went Wrong");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <div className="login-container">
@@ -20,6 +57,7 @@ const AccountLogin = () => {
 
         <div className="input-filed">
           <input
+            disabled={isSubmitting}
             ref={emailRef}
             type="text"
             placeholder="Email or Phone"
@@ -40,6 +78,7 @@ const AccountLogin = () => {
 
         <div className="input-filed">
           <input
+            disabled={isSubmitting}
             ref={passwordRef}
             type="password"
             placeholder="Password"
@@ -50,11 +89,12 @@ const AccountLogin = () => {
             onBlur={(event) => {
               event.target.style.border = "none";
             }}
-            onChange={handleChange}
           />
         </div>
         <div className="login-btn">
-          <button>Log In</button>
+          <button disabled={isSubmitting} onClick={handleSubmit}>
+            Log In
+          </button>
         </div>
         <div className="register">
           Don't have account ?{" "}
@@ -62,6 +102,10 @@ const AccountLogin = () => {
             Register Here{" "}
           </Link>
         </div>
+
+        {Boolean(validationErrors) && (
+          <div style={{ color: "red", width: "max-content", position: "relative", top: "70px", left: "28px", overflow: "hidden", display: "inline-block", fontSize: "18px" }}>{validationErrors}</div>
+        )}
       </div>
     </div>
   );
--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,16 +1,52 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import "./register.css";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import { isEmail } from "../RegEx";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+axios.interceptors.request.use(function (config) {
+  config.headers["X-Binarybox-Api-Key"] = process.env.REACT_APP_API_KEY;
+  return config;
+});
+
 const AccountRegister = () => {
+  const navigate = useNavigate();
+  const [validationErrors, setValidationErrors] = useState("");
   const nameRef = useRef("");
   const emailRef = useRef(" ");
   const passwordRef = useRef("");
   const confirmPasswordRef = useRef("");
 
-  const handleClick = () => {
-    const userEmail = isEmail(emailRef.current.value);
+  const handleRegister = () => {
+    const isEmailValid = isEmail(emailRef.current.value);
+    const isPasswordMatched = passwordRef.current.value === confirmPasswordRef.current.value;
+
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: confirmPasswordRef.current.value,
+    };
+
+    if (isEmailValid && isPasswordMatched) {
+      axios
+        .post("/api/register", payload)
+        .then((Response) => {
+          setValidationErrors("");
+          localStorage.setItem("credentials", JSON.stringify({ userAuth: Response.data, credentials: payload }));
+          localStorage.setItem("token", Response.data.token);
+          navigate("/login");
+        })
+        .catch((error) => {
+          if (error.response.data.message !== undefined) {
+            setValidationErrors(error.response.data.message);
+          }
+        });
+    } else {
+      setValidationErrors("Something went wrong, please verify your details.");
+    }
   };
 
   return (
@@ -83,7 +119,7 @@ const AccountRegister = () => {
         </div>
 
         <div className="Register-btn">
-          <button aria-label="register-button" onClick={handleClick}>
+          <button aria-label="register-button" onClick={handleRegister}>
             Register
           </button>
         </div>
@@ -94,6 +130,9 @@ const AccountRegister = () => {
             Login
           </Link>
         </div>
+        {Boolean(validationErrors) && (
+          <div style={{ color: "red", width: "max-content", position: "relative", top: "70px", left: "28px", overflow: "hidden", display: "inline-block", fontSize: "18px" }}>{validationErrors}</div>
+        )}
       </div>
     </div>
   );
