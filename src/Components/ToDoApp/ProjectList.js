aa14c0d37777a2dde5af5978504ef603972ae0f8
Chandan <Chandan17ee@gmail.com>
2024-04-05 21:21:16 +0530

    Fix: fixed linting

--- src/Components/ToDoApp/ProjectList.js
+++ src/Components/ToDoApp/ProjectList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "../../Common/Modal/Modal";
 import "./todo.css";
 import Button from "../../Common/ButtonComponent/Button";
