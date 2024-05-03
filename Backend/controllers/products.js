716baec5c6680d3aed58debe6430798c118391c9
Chandan <Chandan17ee@gmail.com>
2024-05-03 14:01:02 +0530

    chore: added new way to throw an error

--- Backend/controllers/products.js
+++ Backend/controllers/products.js
@@ -19,7 +19,9 @@ const fetchAllProducts = asyncHandler(async (req, res, next) => {
     return res.status(200).json(stringifyData(response.data));
   } catch (err) {
     res.status(400);
-    throw new Error("Something Went Wrong...");
+    throw new Error("Something Went Wrong..."); // Here we are using throw method, so that our error handler can detect the error.
+
+    //  Note : if you pass like :  res.status(400).send("something bad")  then our error handler middleware ca not detect it.
   }
 });
 
