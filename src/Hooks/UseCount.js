0e7d2f3a493a36a76ec9f6ff89bac40931d29be6
Chandan <Chandan17ee@gmail.com>
2024-05-31 23:52:28 +0530

    Chore: integrated usetoggle and usecounter hooks

--- src/Hooks/UseCount.js
+++ src/Hooks/UseCount.js
@@ -1,7 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
-const UseCounter = () => {
-  return <div>UseCounter</div>;
+import { useState, useCallback } from "react";
+
+const UseCounter = (initialValue) => {
+  const [counter, setCounter] = useState(initialValue ?? 0);
+
+  const increment = useCallback(() => {
+    setCounter((prevCount) => prevCount + 1);
+  }, []);
+
+  const decrement = useCallback(() => {
+    setCounter((prevCount) => prevCount - 1);
+  }, []);
+
+  const reset = useCallback(() => {
+    setCounter(initialValue ?? 0);
+  }, [initialValue]);
+
+  return [counter, setCounter, increment, decrement, reset];
 };
 
 export default UseCounter;
--- src/Hooks/UseToggle.js
+++ src/Hooks/UseToggle.js
@@ -1,48 +1,15 @@
-import { useCallback, useState } from "react";
+// Custom hook that manages a boolean toggle state in React components.
 
-const useToggle = (values, startIndex = 0) => {
-  // to track the indexes
-  const [index, setIndex] = useState(startIndex);
+import { useState, useCallback } from "react";
 
-  // define and memorize the toggler function in case we pass down the component,
-  // this will move the index to the next level and reset it if it goes beyond the limit.
-  const toggle = useCallback(() => setIndex((prevIndex) => (prevIndex >= values.length - 1 ? 0 : prevIndex + 1)), [values]);
+const useToggle = (defaultValue = false) => {
+  const [value, setValue] = useState(defaultValue);
 
-  // return value and toggle function
-  return [values[index], toggle];
+  const toggleFunction = useCallback(() => {
+    setValue((prevValue) => !prevValue);
+  }, []);
+
+  return [value, setValue, toggleFunction];
 };
 
 export default useToggle;
-
-//  NOTE :
-
-/**
- * the useToggle() hook in React that accepts an array of values and the start index and toggles the next value in the array.
- *  If the index reaches to the last index of the array, reset the index.
-
-This hook will return the current value and the toggle() method using which we can toggle the values.
-
-To create this hook all we need to do is track the indexes and
- we can use the index to return the value from the array thus use the useState() hook to persist and track indexes and then use the useCallback() hook to create the toggle function.
-
-By using the useCallback() hook, we can memorize the toggle method so that it can be passed down through components.
- */
-
-//        Implementation Example
-
-/**
-  function Example() {
-  // call the hook which returns, the current value and the toggled function
-  const [currentValue, toggleValue] = useToggle(["a", "b", "c", "d"], 2);
-  return <button onClick={toggleValue}> "currentValue" : {currentValue}</button>;
-}
-
-export default Example;
-
-Output:
-currentValue: c // initially
-currentValue: d // onClick
-currentValue: a // onClick
-currentValue: b // onClick
-currentValue: c // onClick
- */
