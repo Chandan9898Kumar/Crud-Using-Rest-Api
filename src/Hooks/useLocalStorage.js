f86be9da89e122578be3dd88a9c9a51cf0e95503
Chandan <Chandan17ee@gmail.com>
2024-06-07 16:14:59 +0530

    chore: implemented uselocalstorage hook

--- src/Hooks/useLocalStorage.js
+++ src/Hooks/useLocalStorage.js
@@ -1,3 +1,5 @@
+//   Creates a stateful value that is persisted to localStorage, and a function to update it.
+
 import { useState, useCallback } from "react";
 const useLocalStorage = (key, initialValue = null) => {
   const [forceUpdate, setForceUpdate] = useState(false);
@@ -63,3 +65,51 @@ export default useLocalStorage;
   );
 }
  */
+// ========================================================================================================================================
+
+
+//                      2nd  example
+
+// 1. Use the useState() hook with a function to initialize its value lazily.
+// 2. Use a try...catch block and Storage.getItem() to try and get the value from Window.localStorage. If no value is found, use Storage.setItem() to store the defaultValue and use it as the initial state. If an error occurs, use defaultValue as the initial state.
+// 3. Define a function that will update the state variable with the passed value and use Storage.setItem() to store it.
+
+// const useLocalStorage = (keyName, defaultValue) => {
+//   const [storedValue, setStoredValue] = React.useState(() => {
+//     try {
+//       const value = window.localStorage.getItem(keyName);
+
+//       if (value) {
+//         return JSON.parse(value);
+//       } else {
+//         window.localStorage.setItem(keyName, JSON.stringify(defaultValue));
+//         return defaultValue;
+//       }
+//     } catch (err) {
+//       return defaultValue;
+//     }
+//   });
+
+//   const setValue = newValue => {
+//     try {
+//       window.localStorage.setItem(keyName, JSON.stringify(newValue));
+//     } catch (err) {}
+//     setStoredValue(newValue);
+//   };
+
+//   return [storedValue, setValue];
+// };
+
+// export default useLocalStorage
+
+// ====================================================================================================================================
+//                                 Example to use above hook
+// const MyApp = () => {
+//   const [name, setName] = useLocalStorage('name', 'John');
+
+//   return <input value={name} onChange={e => setName(e.target.value)} />;
+// };
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <MyApp />
+// );
