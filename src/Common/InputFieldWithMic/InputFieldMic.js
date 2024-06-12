26b64531f2058703a74c4b2f722cba7f9a3f0018
Chandan <Chandan17ee@gmail.com>
2024-06-12 14:52:25 +0530

    chore:added table

---
+++ src/Common/InputFieldWithMic/InputFieldMic.js
@@ -0,0 +1,24 @@
+import React, { memo, useState } from "react";
+import "./mic.css";
+const InputFieldMic = ({ onChange, value }) => {
+  return (
+    <div className="InputContainer">
+      <input type="text" name="text" className="input" id="input" placeholder="Search" />
+
+      <label htmlFor="input" className="labelforsearch">
+        <svg viewBox="0 0 512 512" className="searchIcon">
+          <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path>
+        </svg>
+      </label>
+      <div className="border"></div>
+
+      <button className="micButton">
+        <svg viewBox="0 0 384 512" className="micIcon">
+          <path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"></path>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default memo(InputFieldMic);
--- src/Components/Contacts/Contact.js
+++ src/Components/Contacts/Contact.js
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React, { lazy, useState, Suspense } from "react";
 const ReUsableTable = lazy(() => import("../../Common/ReUsableTable/ReUsableTable"));
 
 const headers = [
@@ -9,8 +9,41 @@ const headers = [
 ];
 
 const Contact = () => {
+  const [search, setSearch] = useState("");
+
+  const onSearch = (event) => {
+    setSearch(event.target.value);
+  };
   return (
-    <>
+    <React.Fragment>
+      <div style={{ padding: "20px 40px" }}>
+        <input
+          style={{
+            height: "35px",
+            outline: "none",
+            width: "100%",
+            maxWidth: "400px",
+            textAlign: "justify",
+            fontSize: "18px",
+            fontWeight: "500",
+            border: "2px solid transparent",
+            borderRadius: "10px",
+            transition: ".5s ease",
+          }}
+          type="text"
+          id="inputText"
+          placeholder="Search"
+          pattern="[A-Za-z]"
+          value={search}
+          onChange={onSearch}
+          onFocus={(event) => {
+            event.target.style.border = " 2px solid rgba(129, 140, 248)";
+          }}
+          onBlur={(event) => {
+            event.target.style.border = "2px solid transparent";
+          }}
+        />
+      </div>
       <div
         style={{
           display: "flex",
@@ -19,10 +52,12 @@ const Contact = () => {
           flexDirection: "column",
         }}
       >
-        <h2>Table Component</h2>
-        <ReUsableTable headers={headers} />
+        <Suspense fallback="Loading...">
+          <h2>Table Component</h2>
+          <ReUsableTable headers={headers} />
+        </Suspense>
       </div>
-    </>
+    </React.Fragment>
   );
 };
 export default Contact;
