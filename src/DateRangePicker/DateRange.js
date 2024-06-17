7157da6b8ddb632eab837fe3644915c1884cbc12
Chandan <Chandan17ee@gmail.com>
2024-06-17 20:53:56 +0530

    Fix: Fixed linting

--- src/DateRangePicker/DateRange.js
+++ src/DateRangePicker/DateRange.js
@@ -1,4 +1,5 @@
 import React, { useState, memo } from "react";
+
 import { DateRangePicker } from "react-date-range";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
