43e761ce064dc3583e170d8d4f444326735c1d16
Chandan <Chandan17ee@gmail.com>
2024-06-28 22:03:37 +0530

    Fix: Fixed linting

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import ProjectComponent from "../Redux/TodoApplication/ProjectListReducer";
+
 import CreateProjectComponent from "../Redux/TodoApplication/CreateProjectReducer";
 import EditProjectComponent from "../Redux/TodoApplication/EditProjectReducer";
+import ProjectComponent from "../Redux/TodoApplication/ProjectListReducer";
 import ShowProjectComponent from "../Redux/TodoApplication/ShowProjectReducer";
 export const store = configureStore({
   reducer: {
