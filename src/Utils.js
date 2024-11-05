1a825b937b7353be8e94ef610a2a985dfc273442
Chandan <Chandan17ee@gmail.com>
2024-11-05 20:13:20 +0530

    chore: added url parser function

--- src/Utils.js
+++ src/Utils.js
@@ -103,7 +103,7 @@ export const throttleV2 = (func, limit) => {
 };
 
 export const slugify = (str = "") => {
-  let slug = str
+  const slug = str
     .toLowerCase()
     .replace(/\s/g, "-")
     .replace(/[^a-zA-Z0-9-]/g, "");
@@ -476,4 +476,38 @@ export const getDaysBetweenDates = (dateText1, dateText2) => {
 
   //return the days
   return diffDays;
+};
+
+//   function to parse an URL.
+
+export const parseURL = (url) => {
+  const a = document.createElement("a");
+  a.href = url;
+  return {
+    source: url,
+    protocol: a.protocol.replace(":", ""),
+    host: a.hostname,
+    port: a.port,
+    query: a.search,
+    params: (function () {
+      let ret = {},
+        seg = a.search.replace(/^\?/, "").split("&"),
+        len = seg.length,
+        i = 0,
+        s;
+      for (; i < len; i++) {
+        if (!seg[i]) {
+          continue;
+        }
+        s = seg[i].split("=");
+        ret[s[0]] = s[1];
+      }
+      return ret;
+    })(),
+    file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ""])[1],
+    hash: a.hash.replace("#", ""),
+    path: a.pathname.replace(/^([^\/])/, "/$1"),
+    relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ""])[1],
+    segments: a.pathname.replace(/^\//, "").split("/"),
+  };
 };
